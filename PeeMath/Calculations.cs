c25b1b8f726b41cbaadb296e7c14041b37bf06fb
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2019-12-19 18:14:55 +0300

    all the functions (summary, subtraction, multiplication, division, square, exponentiation, ln, log, sin, cos, tg, ctg, arcsin, arccos, arctg, arcctg, squareroot, root) + currencies (from rubbles to dollars, from rubbles to euro etc.), transfer from common number system to chosen, currencies with time (from days to hours, from days to minutes etc.)

---
+++ PeeMath/Calculations.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeeMath
+{
+    class Calculations
+    {
+        public double Summary(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Division(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double Square(double a)
+        {
+            return a * a;
+        }
+
+        public double Exponentiation(double a, int b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double Ln(double a)
+        {
+            return Math.Log(a);
+        }
+
+        public double Log(double a, double b)
+        {
+            return Math.Log(a, b);
+        }
+
+        public double Sin(double a)
+        {
+            return Math.Sin(a * Math.PI / 180);
+        }
+
+        public double Cos(double a)
+        {
+            return Math.Cos(a * Math.PI / 180);
+        }
+
+        public double Tg(double a)
+        {
+            return Math.Tan(a * Math.PI / 180);
+        }
+
+        public double Ctg(double a)
+        {
+            return 1.0 / Math.Tan(a * Math.PI / 180);
+        }
+
+        public double Arcsin(double a)
+        {
+            return Math.Asin(a);
+        }
+
+        public double Arccos(double a)
+        {
+            return Math.Acos(a);
+        }
+
+        public double Arctg(double a)
+        {
+            return Math.Atan(a);
+        }
+
+        public double Arcctg(double a)
+        {
+            return Math.PI / 2 - Math.Atan(a);
+        }
+
+        public double SquareRoot(double a)
+        {
+            return Math.Sqrt(a);
+        }
+
+        public double Root(double a, int b)
+        {
+            return Math.Pow(a, 1 / b);
+        }
+
+
+        
+    }
+}
---
+++ PeeMath/Currencies.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeeMath
+{
+    class Currencies
+    {
+        double usdValue;
+        double euroValue;
+
+        public Currencies(double _usdValue, double _euroValue)
+        {
+            usdValue = _usdValue;
+            euroValue = _euroValue;
+        }
+
+        public double FromDaysToHours(double a)
+        {
+            return a * 24;
+        }
+
+        public double FromHoursToDays(double a)
+        {
+            return a / 24;
+        }
+
+        public double FromDaysToMinutes(double a)
+        {
+            return a * 24 * 60;
+        }
+        
+        public double FromMinutesToDays(double a)
+        {
+            return a / (24 * 60);
+        }
+
+        public double FromDaysToSeconds(double a)
+        {
+            return a * 24 * 60 * 60;
+        }
+
+        public double FromSecondsToDays(double a)
+        {
+            return a / (24 * 60 * 60);
+        }
+
+        public double FromHoursToMinutes(double a)
+        {
+            return a * 60;
+        }
+
+        public double FromMinutesToHours(double a)
+        {
+            return a / 60;
+        }
+
+        public double FromHoursToSeconds(double a)
+        {
+            return a * 60 * 60;
+        }
+
+        public double FromSecondsToHours(double a)
+        {
+            return a / (60 * 60);
+        }
+
+        public double FromMinutesToSeconds(double a)
+        {
+            return a * 60;
+        }
+
+        public double FromSecondsToMinutes(double a)
+        {
+            return a / 60;
+        }
+
+
+        public double FromDegreeToRadians(double a)
+        {
+            return a * Math.PI / 180;
+        }
+
+        public double FromRadiansToDegree(double a)
+        {
+            return a * 180 / Math.PI;
+        }
+
+        public string From10toChosen(int a, int b)
+        {
+            string result = "";
+            int temp = 0;
+
+            if (a > 0)
+                while (a >= (b - 1))
+                {
+                    temp = a % b;
+                    a = (a - temp) / b;
+                    result = Convert.ToString(temp) + result;
+                }
+
+            result = Convert.ToString(a) + result;
+            return result;
+        }
+
+
+        public double FromRubToDol(double a)
+        {
+            return a / usdValue;
+        }
+
+        public double FromRubToEuro(double a)
+        {
+            return a / euroValue;
+        }
+
+        public double FromEuroToRub(double a)
+        {
+            return a * euroValue;
+        }
+
+        public double FromDolToRub(double a)
+        {
+            return a * usdValue;
+        }
+    }
+}
