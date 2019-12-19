083e4b0ccf9e5d3dfe56e4432a146f88eb0aec0c
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2019-12-19 19:08:10 +0300

    all the functions (summary, subtraction, multiplication, division, square, exponentiation, ln, log, sin, cos, tg, ctg, arcsin, arccos, arctg, arcctg, squareroot, root) + currencies (from rubbles to dollars, from rubbles to euro etc.), transfer from common number system to chosen, currencies with time (from days to hours, from days to minutes etc.)

--- PeeMath/Calculations.cs
+++ PeeMath/Calculations.cs
@@ -1,102 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PeeMath
 {
     class Calculations
     {
-        public double Summary(double a, double b)
+        public static double Summary(double a, double b)
         {
             return a + b;
         }
 
-        public double Subtraction(double a, double b)
+        public static double Subtraction(double a, double b)
         {
             return a - b;
         }
 
-        public double Multiplication(double a, double b)
+        public static double Multiplication(double a, double b)
         {
             return a * b;
         }
 
-        public double Division(double a, double b)
+        public static double Division(double a, double b)
         {
             return a / b;
         }
 
-        public double Square(double a)
+        public static double Square(double a)
         {
             return a * a;
         }
 
-        public double Exponentiation(double a, int b)
+        public static double Exponentiation(double a, int b)
         {
             return Math.Pow(a, b);
         }
 
-        public double Ln(double a)
+        public static double Ln(double a)
         {
             return Math.Log(a);
         }
 
-        public double Log(double a, double b)
+        public static double Log(double a, double b)
         {
             return Math.Log(a, b);
         }
 
-        public double Sin(double a)
+        public static double Sin(double a)
         {
             return Math.Sin(a * Math.PI / 180);
         }
 
-        public double Cos(double a)
+        public static double Cos(double a)
         {
             return Math.Cos(a * Math.PI / 180);
         }
 
-        public double Tg(double a)
+        public static double Tg(double a)
         {
             return Math.Tan(a * Math.PI / 180);
         }
 
-        public double Ctg(double a)
+        public static double Ctg(double a)
         {
             return 1.0 / Math.Tan(a * Math.PI / 180);
         }
 
-        public double Arcsin(double a)
+        public static double Arcsin(double a)
         {
             return Math.Asin(a);
         }
 
-        public double Arccos(double a)
+        public static double Arccos(double a)
         {
             return Math.Acos(a);
         }
 
-        public double Arctg(double a)
+        public static double Arctg(double a)
         {
             return Math.Atan(a);
         }
 
-        public double Arcctg(double a)
+        public static double Arcctg(double a)
         {
             return Math.PI / 2 - Math.Atan(a);
         }
 
-        public double SquareRoot(double a)
+        public static double SquareRoot(double a)
         {
             return Math.Sqrt(a);
         }
 
-        public double Root(double a, int b)
+        public static double Root(double a, int b)
         {
             return Math.Pow(a, 1 / b);
         }
 
-
         
+
     }
 }
